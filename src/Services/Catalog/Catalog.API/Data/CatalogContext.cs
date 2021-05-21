28f5e33cd39a2329b3a165942f2b7d9b8a38fe71
Juan Felipe Varner <felipe0528@gmail.com>
2021-05-21 08:53:05 -0500

    Data and Repository Layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetValue<String>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(config.GetValue<String>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(config.GetValue<String>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
